c4067ef5972270147b90ee9545478fb05f0f146f
arben-systems3000 <AGrepi@systems3000.com>
2025-06-10 10:25:28 +0200

    made sure google-services is tracked by git, changed babel config

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "@babel/plugin-transform-class-properties",
-      "@babel/plugin-transform-nullish-coalescing-operator",
-      "@babel/plugin-transform-optional-chaining",
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
