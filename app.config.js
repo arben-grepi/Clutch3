379e4889032c3a1a53ce0af441a2143aaa895d78
arben-grepi <arben.grepi@gmail.com>
2025-11-02 23:50:21 +0100

    Löysin MANAGE_EXTERNAL_STORAGE-luvan app.config.js-tiedostosta. Tämä ei ole tarpeen Clutch3:lle, ja se aiheuttaa Google Play -valvonnan ongelmia.

--- app.config.js
+++ app.config.js
@@ -51,7 +51,6 @@ export default {
       "android.permission.READ_MEDIA_VIDEO",
       "android.permission.READ_EXTERNAL_STORAGE",
       "android.permission.WRITE_EXTERNAL_STORAGE",
-      "android.permission.MANAGE_EXTERNAL_STORAGE",
     ],
     googleServicesFile:
       process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
